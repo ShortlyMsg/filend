9571f2e9d6baa25d857356098e2ae9fcdf677db5
MSG <42msg42@gmail.com>
2025-03-09 11:33:10 +0300

    firebase init func added

--- filend-ui/src/main.js
+++ filend-ui/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import './styles/tailwind.css';
+import { initializeFirebase } from "./utils/firebase";
 
-
+initializeFirebase();
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
--- filend-ui/src/utils/firebase.js
+++ filend-ui/src/utils/firebase.js
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import firebaseConfig from "@/config/firebaseConfig.json";
 
-const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
+let messaging = null
+export function initializeFirebase(){
+    const firebaseApp = initializeApp(firebaseConfig);
+    messaging = getMessaging(firebaseApp);
+
+    console.log("Firebase initialized:", firebaseApp);
+    console.log("Messaging initialized:", messaging);
+}
+
 
 export { messaging, getToken, onMessage };
